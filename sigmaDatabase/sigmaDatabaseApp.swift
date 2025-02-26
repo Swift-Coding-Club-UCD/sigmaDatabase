1f7e30c73534caa8457b8d690e28624effa519a7
Lethienbao2004 <markle1903@icloud.com>
2025-02-26 12:08:33 -0800

    Initial Commit

---
+++ sigmaDatabase/sigmaDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigmaDatabaseApp.swift
+//  sigmaDatabase
+//
+//  Created by Mark Le on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct sigmaDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
